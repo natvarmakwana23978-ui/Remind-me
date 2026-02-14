6ca2328d90e0d1a8bac59c891558455f909f2e6c
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-14 17:36:31 +0530

    Update MainActivity.kt

--- app/src/main/java/com/natvar/remindme/MainActivity.kt
+++ app/src/main/java/com/natvar/remindme/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.natvar.remindme
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.GridView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,38 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val calendarGrid: GridView = findViewById(R.id.calendarGrid)
-        
-        // ફેબ્રુઆરી ૨૦૨૬ માટે ૧ થી ૨૮ તારીખો (અંગ્રેજીમાં)
-        val dates = Array(28) { (it + 1).toString() }
+        val recyclerView: RecyclerView = findViewById(R.id.calendarRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val adapter = object : ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dates) {
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-                val view = super.getView(position, convertView, parent) as TextView
-                
-                // દરેક ખાનાની ડિઝાઇન
-                view.setBackgroundColor(Color.WHITE)
-                view.textSize = 18f
-                view.setTextColor(Color.BLACK)
-                view.gravity = Gravity.CENTER
-                
-                // ખાનાની ઊંચાઈ સેટ કરવી (જેથી કેલેન્ડર ચોરસ દેખાય)
-                val params = view.layoutParams ?: ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT, 
-                    ViewGroup.LayoutParams.WRAP_CONTENT
-                )
-                view.layoutParams = params
-                view.height = 150 // પિક્સેલ્સમાં ઊંચાઈ
-                
-                // રવિવારની તારીખ (1, 8, 15, 22) લાલ રંગમાં
-                if (position % 7 == 0) {
-                    view.setTextColor(Color.RED)
-                }
-                
-                return view
-            }
+        // ૨૦૨૬ ના ૧૨ મહિનાની યાદી
+        val monthsList = mutableListOf<Calendar>()
+        for (i in 0..11) {
+            val calendar = Calendar.getInstance()
+            calendar.set(2026, i, 1)
+            monthsList.add(calendar)
         }
 
-        calendarGrid.adapter = adapter
+        // અહીં આપણે એડેપ્ટર સેટ કરીશું (જે કામ હું તમને આગળ આપીશ)
+        // recyclerView.adapter = CalendarAdapter(monthsList)
     }
 }
