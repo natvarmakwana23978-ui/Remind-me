95d543085766b91066af95c06951e3ca00078453
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-14 13:52:22 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,35 @@
+plugins {
+    id("com.android.application") version "8.1.1"
+    id("org.jetbrains.kotlin.android") version "1.8.10"
+}
+
+android {
+    namespace = "com.natvar.remindme"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.natvar.remindme"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
 
