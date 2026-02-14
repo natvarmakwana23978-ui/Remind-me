03ee2cae479e8ddeea229be9e4d0d45876ff0f61
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-14 17:41:08 +0530

    Update MainActivity.kt

--- app/src/main/java/com/natvar/remindme/MainActivity.kt
+++ app/src/main/java/com/natvar/remindme/MainActivity.kt
@@ -12,18 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // XML માંથી RecyclerView ને જોડવું
         val recyclerView: RecyclerView = findViewById(R.id.calendarRecyclerView)
+        
+        // લિસ્ટને ઉપર-નીચે (Vertical) સ્ક્રોલ કરવા માટે સેટિંગ
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        // ૨૦૨૬ ના ૧૨ મહિનાની યાદી
+        // ૨૦૨૬ ના ૧૨ મહિના (જાન્યુઆરી થી ડિસેમ્બર) તૈયાર કરવા
         val monthsList = mutableListOf<Calendar>()
         for (i in 0..11) {
             val calendar = Calendar.getInstance()
-            calendar.set(2026, i, 1)
+            calendar.set(2026, i, 1) // વર્ષ ૨૦૨૬, મહિનો i, અને તારીખ ૧
             monthsList.add(calendar)
         }
 
-        // અહીં આપણે એડેપ્ટર સેટ કરીશું (જે કામ હું તમને આગળ આપીશ)
-        // recyclerView.adapter = CalendarAdapter(monthsList)
+        // એડેપ્ટર દ્વારા ડેટાને સ્ક્રીન પર બતાવવો
+        recyclerView.adapter = CalendarAdapter(monthsList)
     }
 }
